3eec9f79f04b855c28d28676abd7717696e3fdd1
Vishnuu <cvishnuu01@gmail.com>
2021-10-26 17:09:36 +0530

    Hashtabe commit 2

--- HashTable.js
+++ HashTable.js
@@ -25,13 +25,34 @@ class HashTable {
         if(currentBucket.length) {
             for(let i = 0; i < currentBucket.length; i++) {
                 if(currentBucket[i][0] === key) {
-                    return currentBucket[i]
+                    return currentBucket[i][1]
                 }
             }
         }
-        return undefined;        
-        
+        return undefined;                
     }
+
+    keys() {
+        if (!this.data.length) {
+          return undefined
+        }
+        let result = []
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+              // but also loop through all the potential collisions
+              if (this.data.length > 1) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                  result.push(this.data[i][j][0])
+                }
+              } else {
+                result.push(this.data[i][0])
+              } 
+            }
+        }
+        return result; 
+      }
 }
 
 const myHashTable = new HashTable(50);
@@ -39,6 +60,9 @@ const myHashTable = new HashTable(50);
 myHashTable.put("grapes", 1000);
 myHashTable.put("apples", 2000);
 
+
+
 console.log(myHashTable.get("grapes"))
 console.log(myHashTable.get("apples"))
 
+console.log(myHashTable.keys())
