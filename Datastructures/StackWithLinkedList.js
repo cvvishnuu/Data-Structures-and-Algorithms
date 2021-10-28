63ddc8979aa13effa4fb20c657756298674616d9
Vishnuu <cvishnuu01@gmail.com>
2021-10-28 20:23:24 +0530

    Stack Implementation(linked list)

--- Datastructures/StackWithLinkedList.js
+++ Datastructures/StackWithLinkedList.js
@@ -13,19 +13,58 @@ class Stack {
     }
 
     peek() {
+        return this.top;
+    }
 
+    peekBottom() {
+        return this.bottom;
     }
 
     push(value) {
-        
+        const node = new Node(value);
+        if(this.length === 0) {
+            this.top = node;
+            this.bottom = node
+        } else {
+            const temp = this.top
+            this.top = node  
+            this.top.next = temp 
+        }
+        this.length++
+        return this        
     }
 
-    pop() {
-
+    pop() {       
+        if(!this.top) {
+            return null
+        }   
+        const node = this.top
+        this.top = this.top.next
+        this.length--
+        return node;
     }
 
     isEmpty() {
-
+        if(this.bottom) {
+            return false
+        }
+        return true
     }
 
 }
+
+const myStack = new Stack() 
+myStack.push(5);
+myStack.push(10);
+myStack.push(15);
+myStack.push(20);
+
+
+
+myStack.push(20);
+myStack.push(15);
+myStack.push("discord");
+console.log(myStack.pop())
+console.log(myStack.peek())
+
+
